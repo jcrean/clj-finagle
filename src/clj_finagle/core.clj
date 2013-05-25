955385aa236c06ddbf62f1909f234219aaf88ff4
Josh <joshua.crean@gmail.com>
2013-05-25 16:36:00 -0400

    better dsl for defining processors/servers/clients/api-calls

--- src/clj_finagle/core.clj
+++ src/clj_finagle/core.clj
@@ -7,10 +7,62 @@
    [com.twitter.finagle.stats InMemoryStatsReceiver]
    [com.twitter.util Future FutureEventListener]))
 
-(defmacro def-processor [name service-type & fn-sigs]
-  `(def ~name
-        (proxy [~(symbol (str service-type "$FutureIface"))] []
-          ~@fn-sigs)))
+(defonce processor-registry (atom {}))
+
+(defonce rpc-registry (atom {}))
+
+(defn rpc-server [service-id]
+  (get-in @rpc-registry [service-id :server]))
+
+(defn rpc-client [service-id]
+  (get-in @rpc-registry [service-id :client]))
+
+(defn shutdown-server [service-id]
+  (-> (rpc-server service-id)
+      (.close)))
+
+(defn register [the-name config]
+  (swap! rpc-registry assoc the-name config))
+
+(defn processor [processor-id]
+  (get @processor-registry processor-id))
+
+(defmacro def-processor [processor-id service-type & fn-sigs]
+  `(swap! processor-registry assoc ~processor-id
+          (proxy [~(symbol (str service-type "$FutureIface"))] []
+            ~@fn-sigs)))
+
+(def stats-receiver-makers
+     {:in-memory
+      (fn [] (InMemoryStatsReceiver.))})
+
+(defn make-stats-receiver [type]
+  (if-let [make-fn (get stats-receiver-makers type)]
+    (make-fn)
+    (throw (RuntimeException. (format "dont know how to make that: %s" type)))))
+
+(defmacro def-rpc [service-id & config]
+  (let [cfg-map (apply hash-map config)]
+    `(let [server#
+           (ServerBuilder/safeBuild
+            (~(symbol (str (:service cfg-map) "$FinagledService.")) (processor ~(:processor cfg-map)) (TBinaryProtocol$Factory.))
+            (.. (ServerBuilder/get)
+                (name   ~(:name cfg-map))
+                (codec  (ThriftServerFramedCodec/get))
+                (bindTo (InetSocketAddress. ~(:port cfg-map)))))
+           client#
+           (~(symbol (str (:service cfg-map) "$FinagledClient."))
+            (ClientBuilder/safeBuild
+             (.. (ClientBuilder/get)
+                 (hosts (InetSocketAddress. ~(:port cfg-map)))
+                 (codec (ThriftClientFramedCodec/get))
+                 (hostConnectionLimit ~(or (:host-connection-limit cfg-map) 1))))
+            (TBinaryProtocol$Factory.)
+            ~(:name cfg-map)
+            (make-stats-receiver (or ~(:stats-receiver cfg-map) :in-memory)))]
+       (register ~service-id {:server server#
+                              :client client#}))))
+
 
 (defmacro def-server [name & config]
   (let [cfg-map (apply hash-map config)]
@@ -21,15 +73,6 @@
                (name   ~(:name cfg-map))
                (codec  (ThriftServerFramedCodec/get))
                (bindTo (InetSocketAddress. ~(:port cfg-map))))))))
-
-(def stats-receiver-makers
-     {:in-memory
-      (fn [] (InMemoryStatsReceiver.))})
-
-(defn- make-stats-receiver [type]
-  (if-let [make-fn (get stats-receiver-makers type)]
-    (make-fn)
-    (throw (RuntimeException. (format "dont know how to make that: %s" type)))))
 
 (defmacro def-client [name & config]
   (let [cfg-map (apply hash-map config)]
@@ -45,11 +88,11 @@
            ~(make-stats-receiver (:stats-receiver cfg-map))))))
 
 
-(defmacro def-api-for [the-client fn-name arglist client-call success-handler failure-handler]
+(defmacro def-client-api [fn-name arglist cfg success-cb failure-cb]
   `(defn ~fn-name ~arglist
-     (.. ~the-client
-         ~client-call
+     (.. (rpc-client ~(:for-service cfg))
+         (~fn-name ~@arglist)
          (addEventListener
           (proxy [FutureEventListener] []
-            ~success-handler
-            ~failure-handler)))))
+            ~success-cb
+            ~failure-cb)))))
