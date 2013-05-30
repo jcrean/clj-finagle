a88162bedd6a69b1f9a9680ff3e5847b3ca21fea
Josh <joshua.crean@gmail.com>
2013-05-30 09:10:58 -0400

    new snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-finagle "0.1.1"
+(defproject clj-finagle "0.1.2-SNAPSHOT"
   :description "Clojure wrappers around Twitter's Finagle"
   :url "http://github.com/jcrean/clj-finagle"
   :license {:name "Eclipse Public License"
